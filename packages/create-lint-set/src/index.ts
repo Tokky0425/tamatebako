c0aeb7bfeeae423e6d8badc2e0c2623823dba835
Tokky0425 <lily.yarn.5@gmail.com>
2024-12-04 20:08:15 +0900

    chore!: node のサポートバージョンを20に変更

--- packages/create-lint-set/src/index.ts
+++ packages/create-lint-set/src/index.ts
@@ -8,7 +8,7 @@ const currentNodeVersion = process.versions.node
 const versions = currentNodeVersion.split('.')
 const majorVersion = Number(versions[0])
 
-if (majorVersion < 14) {
+if (majorVersion < 20) {
   console.error(chalk.red('Requires Node 14 or higher.'))
   process.exit(1)
 }
